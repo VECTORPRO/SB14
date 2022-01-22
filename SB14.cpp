6c6b9784614a1d69fffdf02986c5bbc14ab515da
VECTORPRO <Vector59rus@gmail.com>
2022-01-22 08:29:33 +0300

    Добавьте файлы проекта.

---
+++ SB14.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <stdint.h>
+#include <iomanip>
+#include <string>
+#include <cstddef>
+
+int main()
+{
+    std::string name("Vorfolomey");
+    std::string Symv1("");
+    std::string SymvLast("");
+
+    std::cout << "String: " << name << "\n";
+    std::cout << "Count Symbols: " << name.length() << "\n";
+
+    std::cout << "First Symbol: " << name.front() << "\n";
+    std::cout << "Last Symbol: " << name.back() << "\n";
+
+    std::cin;
+    return 0;
+}
